13b6a172cafb9971adf7aba38571b924e0ae1451
liusy58 <liusy58@mail2.sysu.edu.cn>
2021-02-22 13:35:20 +0800

    finish fs

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -309,6 +309,28 @@ sys_open(void)
       return -1;
     }
     ilock(ip);
+    int cnt =10;
+    while (ip->type==T_SYMLINK&&!(omode&O_NOFOLLOW)&&cnt)
+    {
+      if(readi(ip,0,(uint64)path,0,ip->size)!=ip->size){
+          iunlockput(ip);
+          end_op();
+          return -1;
+      }
+      iunlockput(ip);
+      if((ip = namei(path)) == 0){
+        end_op();
+        return -1;
+      }
+      ilock(ip);
+      cnt--;
+    }
+    if(!cnt){
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
+    
     if(ip->type == T_DIR && omode != O_RDONLY){
       iunlockput(ip);
       end_op();
@@ -482,5 +504,27 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+uint64 sys_symlink(void){
+  char new[MAXPATH], old[MAXPATH];
+  struct inode *dp;
+
+  if(argstr(0, old, MAXPATH) < 0 || argstr(1, new, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+  dp = create(new,T_SYMLINK,0,0);
+  if(dp == 0){
+    end_op();
+    return -1;
+  }
+  if(writei(dp,0,(uint64)old,0,MAXPATH)!=MAXPATH){
+    iunlockput(dp);
+    end_op();
+    return -1;
+  }
+  iunlockput(dp);
+  end_op();
   return 0;
 }
